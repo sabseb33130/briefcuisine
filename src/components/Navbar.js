55f7af8cbdeedcb18c2f3eaacb9ec26e07dd1c07
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2022-12-15 15:55:05 +0100

    feat:correction erreur liés au console.log

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,11 @@ import './Navbar.css';
 export default function Navbar() {
     return (
         <div>
-            <nav class="navbar">
-                <form class="container-fluid justify-content-start">
-                    <button class="btn btn-dark me-2" type="button">Accueil</button>
-                    <button class="btn btn-dark me-2" type="button">Catégories</button>
-                    <button class="btn btn-dark me-2" type="button">Favories</button>
+            <nav className="navbar">
+                <form className="container-fluid justify-content-start">
+                    <button className="btn btn-dark me-2" type="button">Accueil</button>
+                    <button className="btn btn-dark me-2" type="button">Catégories</button>
+                    <button className="btn btn-dark me-2" type="button">Favories</button>
                 </form>
             </nav>
             <h1 className='display-3 text-center'>RECETTES MORTELLES <img src='favicon.png'alt='logo'/></h1>
