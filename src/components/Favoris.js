75fc82e320578d0cc9fc8c7c37083b13b8d9378b
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2022-12-15 15:54:11 +0100

    feat:correction erreur liés au console.log

--- src/components/Favoris.js
+++ src/components/Favoris.js
@@ -3,12 +3,10 @@
 export function Favoris() {
   const stockage1 = JSON.parse(localStorage.getItem("stockage"));
 
-  console.log(stockage1);
-  console.log(stockage1.meals[0].strMeal);
 
   return (
     <div>
-      <p>{stockage1.meals[0].strMeal}</p>
+      
     </div>
   );
 }
