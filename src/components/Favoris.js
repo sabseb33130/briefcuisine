b903a0db8d6c3de016dea58b65f951c5b9bb7a3b
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2022-12-19 11:51:48 +0100

    merge

--- src/components/Favoris.js
+++ src/components/Favoris.js
@@ -1,10 +1,13 @@
+
+
 export function Favoris({ sendRecipe }) {
 
   try{ 
     const stockage = JSON.parse(localStorage.getItem("stockage"));
 //Récupération du localStorage et traitement visuel des infos stockées
+ 
   const vignetteFavori = stockage.map((data, i) => (
-    <div
+    <div><div
       onClick={(e) => sendRecipe(data.recipe.meals[0].idMeal)}
       key={i}
       className="card m-2 w-33 col-4"
@@ -15,13 +18,14 @@ export function Favoris({ sendRecipe }) {
         src={data.recipe.meals[0].strMealThumb}
         className="card-img-top"
         alt="{data.recipe.meals[0].strMeal"
-      />
+      /></div>
+     <button>videz les favoris</button> 
     </div>
   ));
 
   return (
     <div className="container justify-content-center mx-auto inline-flex row row-cols-2 ">
-      {vignetteFavori}
+    {vignetteFavori}
     </div>
   );
 } catch{
