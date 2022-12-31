3814f26c6d52d75cbdf9c0ac46ab264a403cf11b
GARITEY Jean Sebastien (SNCF RESEAU / INFRAPOLE AQUITAINE / UP4 CSA) <7604674a@commun.ad.sncf.fr>
2022-12-31 09:38:01 +0100

    fix correction de mon erreur de merge

--- src/components/Favoris.js
+++ src/components/Favoris.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export function Favoris({ sendRecipe }) {
-
   const [noFav, setNoFav] = useState("");
   const [clear, setClear] = useState(false);
 
@@ -9,60 +8,74 @@ export function Favoris({ sendRecipe }) {
     const stockage = JSON.parse(localStorage.getItem("stockage"));
     //Récupération du localStorage et traitement visuel des infos stockées
     const vignetteFavori = stockage.map((data, i) => (
-      <div><div
-        onClick={(e) => { sendRecipe(data.recipe.meals[0].idMeal) }}
-        key={i}
-        className="card m-2 w-33 col-4"
-        style={{ width: 21 + "rem", cursor: "pointer" }}
-      >
-        <h3>{data.recipe.meals[0].strMeal}</h3>
-        <img
-          src={data.recipe.meals[0].strMealThumb}
-          className="card-img-top"
-          alt="{data.recipe.meals[0].strMeal"
-        /></div>
+      <div>
+        <div
+          onClick={(e) => {
+            sendRecipe(data.recipe.meals[0].idMeal);
+          }}
+          key={i}
+          className="card m-2 w-60 col-lg-8 col-sm-12"
+          style={{ cursor: "pointer" }}
+        >
+          <h3>{data.recipe.meals[0].strMeal}</h3>
+          <img
+            src={data.recipe.meals[0].strMealThumb}
+            className="card-img-top"
+            alt="{data.recipe.meals[0].strMeal"
+          />
+        </div>
         <div className="text-center">
-          <button className="btn btn-warning" onClick={(e) => setNoFav(data.no)}>Supprimer ce Favori</button>
+          <button
+            className="btn btn-warning btn-sm"
+            onClick={(e) => setNoFav(data.no)}
+          >
+            Supprimer ce Favori
+          </button>
         </div>
       </div>
     ));
 
     if (noFav !== "") {
-
       const deleteFav = (no) => {
         const copyStockage = [...stockage];
 
-        copyStockage.splice(no,1);
+        copyStockage.splice(no, 1);
 
-        localStorage.setItem("stockage", JSON.stringify(copyStockage))
-      }
-      deleteFav(noFav)
+        localStorage.setItem("stockage", JSON.stringify(copyStockage));
+      };
+      deleteFav(noFav);
     }
 
     if (clear !== false) {
       function clearAll() {
-        localStorage.clear()
+        localStorage.clear();
       }
-      clearAll()
+      clearAll();
     }
-
 
     return (
       <div>
-        <div className="text-center"><button className="m-2 btn btn-danger" onClick={(e) => setClear(true)}>Supprimez vos Favoris</button></div>
-      <div className="container justify-content-center mx-auto inline-flex row row-cols-2 ">
-        
-        {vignetteFavori}
-        <div>
-          
+        <div className="text-center">
+          <button
+            className="m-2 btn btn-danger"
+            onClick={(e) => setClear(true)}
+          >
+            Supprimez vos Favoris
+          </button>
         </div>
-      </div></div>
+        <div className="container justify-content-center mx-auto inline-flex row row-cols-2 ">
+          {vignetteFavori}
+          <div></div>
+        </div>
+      </div>
     );
   } catch {
     return (
       <div className="text-center m-3">
         <meta http-equiv="refresh" content="5"></meta>
-        <strong className="text-danger fs-2">Pas de favoris pour l'instant !!</strong>
+        <strong className="text-danger fs-2">
+          Pas de favoris pour l'instant !!
+        </strong>
         <p>La page va se recharger toute seule !</p>
       </div>
     );
